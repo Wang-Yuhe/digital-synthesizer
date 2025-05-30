f794d3ad80a38a26a752c06fd38259f8b482eb04
tangyayan <tangyayantl@126.com>
2025-05-30 21:52:01 +0800

    修改了audio文件，修改了timbre分析的注释

--- src/timbre/timbre_analysis_test.py
+++ src/timbre/timbre_analysis_test.py
@@ -3,33 +3,30 @@ import librosa
 import scipy.signal
 from pathlib import Path
 
-def timbre_analysis_dict(
+def timbre_analysis_list(
     filename: str,
     frame_length: int = 2048,#窗口大小
     hop_length: int = 512,#移动步长
-    n_harmonics: int = 10,
+    n_harmonics: int = 16,
     fmin: float = 50,
     fmax: float = 3000
-) -> dict[float, list[float]]:
+) -> tuple[list[str], list[list[float]]]:
     """
     对旋律或多音（单音/多音）音频：
-      1) 降噪
-      2) 逐帧基频跟踪（YIN）
-      3) 提取每个基频的前 n_harmonics 谐波幅度并归一化
-    返回：
-      一个 dict，key = f0（Hz），value = [h1_rel, h2_rel, …, hN_rel]
+      1) 逐帧基频跟踪(YIN)
+      2) 提取每个基频的前 n_harmonics 谐波幅度并归一化
+    return:
+      两个list,对应音名和谐波成分
     """
     y, sr = librosa.load(filename, sr=None)
 
     # 基频跟踪
     f0_series, voiced_flag, voiced_prob = librosa.pyin(y, fmin=fmin, fmax=fmax, 
                     sr=sr,frame_length=frame_length, hop_length=hop_length)#pyin效果更好
-    
     S = np.abs(librosa.stft(y, n_fft=frame_length, hop_length=hop_length))#短时傅里叶变换
     #S[f, t]第t帧频率f上的振幅
     freqs = np.linspace(0, sr/2, num=S.shape[0])
 
-    # 遍历每帧，构建 {f0: [harmonics…]} 字典
     result = []
     result_notename = []
     for t, f0 in enumerate(f0_series):
@@ -39,7 +36,7 @@ def timbre_analysis_dict(
         for h in range(1, n_harmonics + 1):
             #原来的谐波成分是找频率点，而事实上有时并不完全为整数倍，这里找范围内峰值最高的
             target_freq = f0 * h
-            search_radius = 30
+            search_radius = 0.05 * target_freq 
             mask = np.where((freqs >= target_freq - search_radius) & (freqs <= target_freq + search_radius))[0]
             
             if len(mask) == 0:
@@ -51,12 +48,13 @@ def timbre_analysis_dict(
             mags.append(local_mags[max_idx_in_window])
 
         mags = np.array(mags)
-        rel = (mags / (mags[0] + 1e-8)).tolist()
-        result_notename.append(librosa.midi_to_note(librosa.hz_to_midi(f0), octave=True))
+        rel = (mags / mags[0]).tolist()
+        result_notename.append(librosa.midi_to_note(librosa.hz_to_midi(f0)))
         result.append(rel)
     
     grouped_names = []
     grouped_results = []
+    if len(result_notename)==0:return [],[]
     current_name = result_notename[0]
     buffer = [result[0]]
     for name, vec in zip(result_notename[1:], result[1:]):
@@ -76,12 +74,12 @@ def timbre_analysis_dict(
 
 if __name__ == "__main__":
     base = Path(__file__).resolve().parent.parent
-    filename = base.parent / 'audio' / 'test.wav'
-    notename, result = timbre_analysis_dict(filename)
-    print(notename)
-    """
+    filename = base.parent / 'audio' / 'harp' / 'harpc5.wav'
+    notename, result = timbre_analysis_list(filename)
+    #print(notename)
+    #"""
     for i in range(len(result)):
         print(f"pitch:{notename[i]}")
         print(result[i])
         print()
-    """
+    #"""
